32293e5c4be416095793aa9dfb9298255f6e038b
Adwaith Renjith <adwaithr2006@gmail.com>
2025-03-03 19:01:24 -0800

    add skeleton

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import pika
+
+# reference
+# https://www.rabbitmq.com/tutorials/tutorial-one-python#using-the-pika-python-client
+
+def order_received(channel, method, properties, body):
+    pass
+
+def main():
+    db_service = os.environ["DB_SERVICE_URL"] # dead-letter-data.dead-letter.svc.cluster.local
+    connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ["MQ_HOST"], os.environ["MQ_PORT"]))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=os.environ["MQ_QUEUE"])
+
+    channel.basic_consume(
+        queue=os.environ["MQ_QUEUE"],
+        auto_ack=True,
+        on_message_callback=order_received,
+    )
+
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
